04f24d32105ace18c0d6959743c71283bc121908
oscarmauricioalvarezgomez <oscarmauricioalvarezgomez@gmail.com>
2024-05-27 13:50:23 +0000

    feat: :sparkles: First template/basic design completed

--- lib/main.dart
+++ lib/main.dart
@@ -25,19 +25,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Icon _heart = Icon(Icons.favorite_border, color: Colors.blue);
+  Icon _heart =
+      Icon(Icons.favorite_border, color: Color.fromARGB(255, 130, 181, 223));
   bool _isLike = false;
 
-   void liked() {
-      setState(() {
-        if (_isLike) {
-          _heart = Icon(Icons.favorite_border, color: Colors.blueAccent);
-          _isLike = !_isLike;
-        } else {
-          _heart = Icon(Icons.favorite, color: Colors.red);
-          _isLike = !_isLike;
-        }
-      });
+  void liked() {
+    setState(() {
+      if (_isLike) {
+        _heart = Icon(Icons.favorite_border,
+            color: const Color.fromARGB(255, 130, 181, 223));
+        _isLike = !_isLike;
+      } else {
+        _heart = Icon(Icons.favorite, color: Colors.red);
+        _isLike = !_isLike;
+      }
+    });
   }
 
   @override
@@ -48,9 +50,32 @@ class _MyHomePageState extends State<MyHomePage> {
         actions: [
           IconButton(
             icon: _heart,
-            onPressed: (){liked();},
+            onPressed: () {
+              liked();
+            },
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          liked();
+        },
+        backgroundColor: Colors.deepPurple,
+        child: _heart,
+        //label: Text("Like..."),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(onPressed: null, icon: Icon(Icons.home)),
+            IconButton(onPressed: null, icon: Icon(Icons.search)),
+            IconButton(onPressed: null, icon: Icon(Icons.add)),
+            IconButton(onPressed: null, icon: Icon(Icons.notifications))
+          ],
+        ),
       ),
       body: Center(
         child: Text("Hello World"),
