48c7c0a210a0df659dcb4e4e464d490448493b6a
oscarmauricioalvarezgomez <oscarmauricioalvarezgomez@gmail.com>
2024-05-27 13:29:14 +0000

    feat: :sparkles: An Appbar with status changes was created.

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-      home: SafeArea(
-    child: MyHelloWorldApp(),
-  ),
-  debugShowCheckedModeBanner: false,
-  debugShowMaterialGrid: false,
-  )
-  );
+  runApp(MyApp());
 }
 
-class MyHelloWorldApp extends StatelessWidget {
-  const MyHelloWorldApp({super.key});
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "ScaffoldApp",
+      home: MyHomePage(title: "ScaffoldApp"),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Icon _heart = Icon(Icons.favorite_border, color: Colors.blue);
+  bool _isLike = false;
+
+   void liked() {
+      setState(() {
+        if (_isLike) {
+          _heart = Icon(Icons.favorite_border, color: Colors.blueAccent);
+          _isLike = !_isLike;
+        } else {
+          _heart = Icon(Icons.favorite, color: Colors.red);
+          _isLike = !_isLike;
+        }
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-        child: Column(
-      children: [
-        Container(
-          height: 50,
-          padding: const EdgeInsets.all(8),
-          decoration: const BoxDecoration(color: Colors.blue),
-          child: const Row(
-            children: [
-              IconButton(
-                  onPressed: null,
-                  icon: Icon(Icons.menu),
-                  tooltip: 'Este Boton te ayudara a navegar'),
-              Expanded(child: Text('Mi Primera App en Flutter desde 0')),
-              IconButton(onPressed: null, icon: Icon(Icons.search))
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title!),
+        actions: [
+          IconButton(
+            icon: _heart,
+            onPressed: (){liked();},
           ),
-        ),
-        const Expanded(
-            child: Center(
-                child: Text(
-          'Hello World! mundo',
-          textDirection: TextDirection.ltr,
-        )))
-      ],
-    ));
+        ],
+      ),
+      body: Center(
+        child: Text("Hello World"),
+      ),
+    );
   }
 }
